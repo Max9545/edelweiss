ae46278729472471173204b98325e1117a4117dc
Max Bregman <bregmanmax91@gmail.com>
2023-08-13 16:01:51 -0600

    Create book-info div for author and catagory and styles for Book

--- src/Book.js
+++ src/Book.js
@@ -7,8 +7,10 @@ function Book ({title, author, cover, category}) {
     <section className='book'>
       {cover === 'No Cover Available' ? <p>No Cover Available At This Time</p> : <img className='cover' src={`${cover}`}/>}
       <h4 className='title'>{title ? `${title}` : `Unknown Title`}</h4>
-      <p className='author'>{author ? `${author}`: `Unknown Author`}</p>
-      <p className='catagory'>{category ? `${category}` : `Unknown Category`}</p>
+      <div className='book-info'>
+        <p className='author'>{author ? `${author}`: `Unknown Author`}</p>
+        <p className='catagory'>{category ? `${category}` : `Unknown Category`}</p>
+      </div>
     </section>
   )
 }
