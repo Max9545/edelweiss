f540908b1cf906c7644f89528ec1d988a5073289
Max Bregman <bregmanmax91@gmail.com>
2023-08-13 15:36:06 -0600

    Create Book component and render book titles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import { useState } from 'react'
 import catalogData from './CatalogInfo'
+import Book from './Book'
 
 function App() {
 
   const [catalog, setCatalog] = useState(catalogData.products)
 
+  const books = catalog.map((book) => {
+    
+    return <Book
+            title={book.name}
+    />
+  })
+
   return (
     <article className="App">
       <h1>Edelweiss Catalog</h1>
+      <div>
+        {books && books}
+      </div>
     </article>
   );
 }
---
+++ src/Book.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Book.css'
+
+function Book ({title}) {
+
+  return(
+    <seaction>
+      <p>{`${title}`}</p>
+    </seaction>
+  )
+}
+
+export default Book
