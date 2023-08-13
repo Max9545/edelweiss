56693e3ae3a45a6af4fab466d088ce9077d2bc08
Max Bregman <bregmanmax91@gmail.com>
2023-08-13 15:47:25 -0600

    Add error handling and classNames to title and author

--- src/Book.js
+++ src/Book.js
@@ -6,8 +6,8 @@ function Book ({title, author, cover}) {
   return(
     <seaction>
       {cover === 'No Cover Available' ? <p>No Cover Available At This Time</p> : <img className='cover' src={`${cover}`}/>}
-      <p>{`${title}`}</p>
-      <p>{`${author}`}</p>
+      <h4 className='title'>{title ? `${title}` : `Unknown Title`}</h4>
+      <p className='author'>{author ? `${author}`: `Unknown Author`}</p>
     </seaction>
   )
 }
