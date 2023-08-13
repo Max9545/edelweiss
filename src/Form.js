7945e96be01611f50566b75097dcfc720d04236e
Max Bregman <bregmanmax91@gmail.com>
2023-08-13 16:17:57 -0600

    Create state for search term and submit button

--- src/Form.js
+++ src/Form.js
@@ -3,14 +3,20 @@ import { useState } from 'react'
 
 function Form () {
 
+  const [searchTerm, setSearchTerm] = useState('')
+console.log(searchTerm)
   return (
     <section>
       <form>
         <label>Search
           <input
             type='text'
+            placeholder='Search Term'
+            name='search'
+            onChange={e => setSearchTerm(e.target.value)}
           />
         </label>
+        <button>SUBMIT</button>
       </form>
     </section>
   )
