9dc80d6c9c5df483d7b586273196163028477ca7
Max Bregman <bregmanmax91@gmail.com>
2023-08-13 15:44:23 -0600

    Render covers and add error handling for missing covers

--- src/Book.js
+++ src/Book.js
@@ -5,7 +5,7 @@ function Book ({title, author, cover}) {
 
   return(
     <seaction>
-      {cover}
+      {cover === 'No Cover Available' ? <p>No Cover Available At This Time</p> : <img className='cover' src={`${cover}`}/>}
       <p>{`${title}`}</p>
       <p>{`${author}`}</p>
     </seaction>
